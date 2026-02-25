3a8ec40beb5f2e12986adcc78fcfd4d02fef933b
DanielValencia <168099985+DanieValencia2005@users.noreply.github.com>
2026-02-24 23:10:45 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ if (!string.IsNullOrEmpty(databaseUrl))
 {
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};Pooling=true;Trust Server Certificate=true";
+    var port = uri.Port != -1 ? uri.Port : 5432; // si no hay puerto, usar 5432
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};Pooling=true;Trust Server Certificate=true";
 }
 else
 {
