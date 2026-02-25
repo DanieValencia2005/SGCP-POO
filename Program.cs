c5565c189604c2a42ebe84c805def1219408dd98
DanielValencia <168099985+DanieValencia2005@users.noreply.github.com>
2026-02-24 22:01:02 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using SGCP_POO.Models;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Obtener la URL de PostgreSQL desde Render
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+// Convertirla a formato que Npgsql entiende
+string connectionString;
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};Pooling=true;Trust Server Certificate=true";
+}
+else
+{
+    // Si no existe la variable, usa tu conexión local
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+
+// Registrar DbContext
 builder.Services.AddDbContext<SGCPContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(connectionString);
 });
 
 builder.Services.AddControllersWithViews();
@@ -20,6 +37,7 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
+// Middlewares
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -37,5 +55,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Escuchar en el puerto de Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.Run();
